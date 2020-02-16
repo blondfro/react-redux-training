b7c160b9ea4684c583b710995f1ebc2cc0090eb7
James Morris <blondfro@gmail.com>
2020-02-16 17:33:37 -0600

    began implementing redux thunk for async management. Also began making CoursesPage a container component and will add in presentational components.

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,15 +1,17 @@
 import { createStore, applyMiddleware, compose } from "redux";
 import rootReducer from "./reducers";
 import reduxImutableStateImvariant from "redux-immutable-state-invariant";
+import thunk from "redux-thunk";
 
 export default function configureStore(initialState) {
   // add support for redux devtools:
-  const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+  const composeEnhancers =
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
   return createStore(
     rootReducer,
     initialState,
-    composeEnhancers(applyMiddleware(reduxImutableStateImvariant()))
+    composeEnhancers(applyMiddleware(thunk, reduxImutableStateImvariant()))
     //this is middleware to warn if state is mutated.
   );
 }
