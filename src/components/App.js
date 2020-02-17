ecf50247ae7daa2c796eabeb01601797382b50f3
James Morris <blondfro@gmail.com>
2020-02-17 12:04:05 -0600

    made use of course form and setup local state for course and error.

--- src/components/App.js
+++ src/components/App.js
@@ -11,15 +11,14 @@ function App() {
   return (
     <div className="container-fluid">
       <Header />
-        <Switch>
-            <Route exact path="/" component={HomePage} />
-            <Route path="/about" component={AboutPage} />
-            <Route path="/courses" component={CoursesPage} />
-            <Route path="/course/:slug" component={ManageCoursePage} />
-            <Route path="/course" component={ManageCoursePage} />
-            <Route component={PageNotFound}/>
-        </Switch>
-
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/about" component={AboutPage} />
+        <Route path="/courses" component={CoursesPage} />
+        <Route path="/course/:slug" component={ManageCoursePage} />
+        <Route path="/course" component={ManageCoursePage} />
+        <Route component={PageNotFound} />
+      </Switch>
     </div>
   );
 }
