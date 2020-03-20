b84386718a0b1cc4354744c52c9d2b217fc38842
James Morris <blondfro@gmail.com>
2020-03-20 13:48:48 -0500

    began configuring Jest.

---
+++ tools/fileMock.js
@@ -0,0 +1,2 @@
+// mocks file imports for jest.  see jest documentation.
+module.exports = "test-file-stub";
---
+++ tools/styleMock.js
@@ -0,0 +1,2 @@
+// tells jest if we import a css file to ignore it on test. see jest documentation.
+module.exports = {};
